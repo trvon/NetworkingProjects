635ebe743a0a1faa475a442353f9dd087db5b8c5
trvon <twill245@uncc.edu>
2017-11-26 08:18:42 -0500

    Committing progress for hw3

--- hw3/main.py
+++ hw3/main.py
@@ -1,5 +1,6 @@
 from multiprocessing import Pool
 from router import Router
+import string
 import time
 
 # Router functionality
@@ -12,17 +13,28 @@ files = [
     "files/f.dat"
 ]
 
-port = [ 9000, 9001, 9002, 9003, 9004, 9005 ]
+# Router ID's
+routerID = [str(i) for i in list(string.ascii_lowercase)[0:6]]
 
+# Routing table with ports and router id's
+table = { 'a' : 9000, 'b' : 9001, 'c' : 9002, 'd' : 9003, 'e' : 9004, 'f' : 9005 }
 
 def router(x):
-    router = Router(x, port[x], files[x])
-    router.readFile()
+    router = Router(routerID[x], table[routerID[x]], files[x], table)
+    router.run()
 
 
 # Main calling code
-with Pool(8) as p:
-    routers = [int(c) for c in range(6)]
+p = Pool(processes=6)
+try:
+    routers = [ int(i) for i in range(6) ]
     processes = p.map(router, routers)
+    p.join()
     # Accessing individual processes
-    # print(processes)
+    print(processes)
+
+# Clean shutdown
+except KeyboardInterrupt:
+    print("\nClean exit")
+    # for i in processes:
+    #   i.close()
--- hw3/router.py
+++ hw3/router.py
@@ -1,8 +1,16 @@
+import socket
+import time
+import sys
+
 class Router:
-	def __init__(self, routerID, port, file):
-		self.port = port
+	def __init__(self, routerID, port, file, table):
+		self.port = int(port)
 		self.file = file
 		self.id = routerID
+		self.routingTable = table
+		del self.routingTable[self.id]
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		# self.sock.bind(('127.0.0.0.1', self.port))
 
 	
 	def readFile(self):
@@ -18,5 +26,19 @@ class Router:
 	def cost(self):
 		bar = 1
 
+	def broadcast(self):
+		foo = 1
+
 	def run(self):
-		foo = 2
+		update = 0
+		while 1:
+			try:
+				self.readFile()
+				self.cost()
+				update += 1
+				time.sleep(15)		# 15 second period
+			except KeyboardInterrupt:
+				break
+
+
+
